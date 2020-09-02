6ccfeeb7e186e0c32eb78bcfcbf4b063c847e9a2
Hyelim <chtr1223@gmail.com>
2020-09-02 22:40:44 +0900

    Update UsbHelper.cs
    test 코드 추가

--- BarcodeReader/UsbHelper.cs
+++ BarcodeReader/UsbHelper.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.IO.Ports;
+using System.Management;
+
 namespace BarcodeReader
 {
     public class UsbHelper
     {
         public UsbHelper()
         {
+            //ManagementObjectCollection collection;
+            //using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+            //    collection = searcher.Get();
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
